a6f80cbd751c5e516187e31933dfd5aeaec6d873
Anton Solovev <69355948+anton-solovev@users.noreply.github.com>
2021-06-12 13:32:27 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,6 +1,6 @@
  
 import telebot
-bot = telebot.TeleBot('1871012202:AAEZKeB1AGmYNty2n5jwftN_yRAMe0kCEfA')
+bot = telebot.TeleBot('Token')
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
